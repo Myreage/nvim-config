ed8264d0358230dcf44c9618cb4055c627704c57
myreage <remy.vandyk@gmail.com>
2024-01-02 15:00:07 +0100

    Handle WSL clipboard

--- init.lua
+++ init.lua
@@ -4,6 +4,22 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- This is to handle Windows WSL clipboard and is breaking elsewise
+-- Remove if necessary
+vim.g.clipboard = {
+	name = 'WslClipboard',
+	copy = {
+		['+'] = 'clip.exe',
+		['*'] = 'clip.exe',
+	},
+	paste = {
+		['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+	},
+	cache_enabled = 0
+}
+
+
 require("lazy-setup")
 require("options")
 require("keymaps")
